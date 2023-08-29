5907248af47bcd23f120977b9401c095baa3c344
riqueschilder <henrique.lima@sou.inteli.edu.br>
2023-08-29 20:14:02 -0300

    delete&update

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, session
+from flask import Flask, request, render_template, redirect, url_for, session, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 import os
@@ -67,6 +67,47 @@ def todolist():
         return render_template('todolist.html', username=username, todos=todos)
     except jwt.ExpiredSignatureError:
         return redirect(url_for('login'))
+
+@app.route('/edit/<int:todo_id>', methods=['POST'])
+def edit_todo(todo_id):
+    token = session.get('token')
+    if not token:
+        return jsonify({"message": "Unauthorized"}), 401
+    
+    try:
+        payload = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        username = payload['username']
+        
+        todo_item = TodoItem.query.get(todo_id)
+        if todo_item and todo_item.username == username:
+            data = request.json
+            new_text = data.get('newText')
+            if new_text:
+                todo_item.todo = new_text
+                db.session.commit()
+                return jsonify({"message": "OK"}), 200
+        return jsonify({"message": "Unauthorized or invalid input"}), 401
+    except jwt.ExpiredSignatureError:
+        return jsonify({"message": "Token expired"}), 401
+
+@app.route('/delete/<int:todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+    token = session.get('token')
+    if not token:
+        return jsonify({"message": "Unauthorized"}), 401
+    
+    try:
+        payload = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        username = payload['username']
+        
+        todo_item = TodoItem.query.get(todo_id)
+        if todo_item and todo_item.username == username:
+            db.session.delete(todo_item)
+            db.session.commit()
+            return jsonify({"message": "OK"}), 200
+        return jsonify({"message": "Unauthorized or invalid input"}), 401
+    except jwt.ExpiredSignatureError:
+        return jsonify({"message": "Token expired"}), 401
 
 if __name__ == '__main__':
     db.create_all()
